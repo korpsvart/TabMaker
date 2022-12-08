a410352cfd18e961f79de295d3c1833843cec239
yan <jeffcorn4books@gmail.com>
2022-12-08 18:36:28 +0100

    feat:add sound play in midi.js

--- src/components/midi.js
+++ src/components/midi.js
@@ -1,4 +1,10 @@
 import {WebMidi} from "webmidi";
+import * as Tone from 'tone'
+const player = new Tone.Synth().toDestination();
+
+let synth;
+let synthNotes = [];
+let synthAllNotes = [];
 
 function enableMidi() {
     WebMidi.enable().then(onEnabled).catch(err => alert(err));
@@ -7,19 +13,37 @@ function enableMidi() {
 function onEnabled() {
     // Display available MIDI input devices
     if (WebMidi.inputs.length < 1) {
-        document.body.innerHTML += "No device detected.";
+        console.log('fail')
     } else {
         console.log('success')
         WebMidi.inputs.forEach((device, index) => {
             console.log(`${index}: ${device.name}`)
         });
     }
-    const mySynth = WebMidi.inputs[0];
-    // const mySynth = WebMidi.getInputByName("TYPE NAME HERE!")
+    synth = WebMidi.inputs[0];
+    addEvent()
+}
 
-    mySynth.channels[1].addListener("noteon", e => {
-        console.log(e.note.octave,e.note.name,e.note.identifier)
+function addEvent(){
+    // every 1s chord
+    synth.channels[1].addListener("noteon", e => {
+        let identifier = e.note.identifier
+        synthAllNotes.push({identifier,time:Date.now()})
+        //play a middle 'C' for the duration of an 8th note
+        player.triggerAttackRelease(identifier, "8n");
     });
 }
 
-export {enableMidi}
+function clearNotesArray(){
+
+}
+
+function getInputNotes(){
+    return synthNotes
+}
+
+function getAllInputNotes(){
+    return synthAllNotes
+}
+
+export {enableMidi,getInputNotes}
